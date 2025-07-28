6290e1eec4cd56ccfcdc891cf7b93a37ec830b3e
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-07-28 14:26:49 +0200

    découverte middleware

--- Demo_ExpressJs/app.js
+++ Demo_ExpressJs/app.js
@@ -1,38 +1,57 @@
 const express = require('express');
-//initialisation d'expressJS
+// Initialisation d'ExpressJS
 const app = express();
-
+ 
 const port = 3000;
-listCities = ['Paris', 'Lyon', 'Rennes', 'Nantes'];
+listCities = ['Paris', 'Lyon', 'Marseille', 'Nantes', 'Rennes'];
 user = {
-    firstname: 'John',
-    lastname: 'Doe'
-};
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+    firstname: "Bob",
+    lastname: "L'Eponge"
+}
+ 
+// Middleware global s'appliquant à toute l'application
+app.use((req, res, next) => {
+    console.log(req.method, req.headers['user-agent']);
+    next();
 });
-
-
-//Route global retournant le tableau de villes
+ 
+// Middleware ne s'appliquant qu'aux routes "/cities/:id" et à leurs dérivés
+app.use('/cities/:id', (req, res, next) => {
+    console.log("Middleware : vérification de l'id")
+    let id = req.params.id;
+    if (id > 0 && id <= listCities.length)
+        next();
+    else
+        res.send("Erreur : id de ville inconnu !");
+});
+ 
+// Route initiale retournant "Hello World!"
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+ 
+// Route globale retournant le tableau de villes
 app.get('/cities', (req, res) => {
     res.send(listCities);
-    });
-
-//Route retournant une ville en fonction de son id
+});
+ 
+// Route retournant une ville spécifique
 app.get('/cities/:id', (req, res) => {
     let id = req.params.id;
-    if (id > 0 && id <= listCities.length) {
-        res.send(listCities[id-1]);
-    } else {
-        res.status(404).send('City not found');
-    }
-    });
-
+    res.send(listCities[id - 1]);
+});
+ 
+// Route retournant un objet JSON
 app.get('/user', (req, res) => {
     res.json(user);
-    });
-
+});
+ 
+// Middleware pour les routes non définies
+app.use((req, res, next) => {
+    res.status(404).send("404 Page Not Found !");
+});
+ 
+// On exécute l'application sur le port spécifié plus haut
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
