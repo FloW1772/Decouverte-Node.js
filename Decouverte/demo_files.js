c19712d75546551b59f9ad0bfbced88b4cdebefc
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-07-28 09:57:07 +0200

    découverte extension mjs

---
+++ Decouverte/demo_files.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// import fs from 'fs';
+
+//méthode asynchrone pour lire un fichier
+fs.readFile('file.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log('Erreur :', err);
+        return;
+    }
+    console.log('Contenu du fichier : ');
+    console.log(data);
+});
+
+
+//méthode synchrone pour lire un fichier
+const data = fs.readFileSync('file.txt', 'utf-8');
+console.log('Contenu du fichier : ');
+console.log(data);
