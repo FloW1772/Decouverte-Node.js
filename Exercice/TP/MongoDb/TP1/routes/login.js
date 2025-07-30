3348b39c9f6c5997ed06d51f58710286a89667e1
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-07-30 16:52:32 +0200

    ajout tp 1 mongodb

---
+++ Exercice/TP/MongoDb/TP1/routes/login.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { body } = require('express-validator');
+const argon2 = require('argon2');
+const { findByEmail } = require('../storage/users');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/', 
+    body('email').isEmail().withMessage('Identifiants invalides !'),
+    body('password').isLength({ min: 1 }).withMessage('Identifiants invalides !'),
+    async (req, res) => {
+        const user = findByEmail(req.body.email);
+        if (user) {
+            if (await argon2.verify(user.password, req.body.password)) {
+                req.session.user = user;
+                req.flash('success', 'Bienvenue ' + user.email + ' !');
+                res.redirect('/');
+            } else {
+                req.flash('error', 'Identifiants invalides !');
+                res.redirect('/login');
+            }
+        } else {
+            req.flash('error', 'Identifiants invalides !');
+            res.redirect('/login');
+        }
+    }
+);
+
+module.exports = router;
