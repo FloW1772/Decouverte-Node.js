b722ca6734cdc0be52221214f8d119043d47f28a
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-07-31 19:35:41 +0200

    ajout de JWT et SWAGGER

---
+++ DEMO/JWT/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const { SECRET } = require('./env');
+
+const user = {
+    id: 'user',
+    pwd: 'password'
+};
+
+function extractBearerToken(header) {
+    const matches = header.match(/(bearer)\s+(\S+)/i);
+    console.log(matches[2]);
+    return matches[2];
+}
+
+function checkTokenMiddleware(req, res, next) {
+    console.log(req.headers.authorization);
+    const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
+
+    if (!token) {
+        return res.status(401).json({ Error: 'No access token ! Try Again hahaha ! '});
+    }
+    jwt.verify(token, SECRET, (err, decodedToken) => {
+        if (err)
+            return res.status(401).json({ Error: 'Bad token ... '});
+        else {
+            return next();
+        }
+    });
+};
+
+app.get('/', (req, res) => {
+    const token = jwt.sign(user, SECRET, { expiresIn: '1 hours'});
+    res.json({ access_token: token });
+});
+
+app.post('/admin', checkTokenMiddleware, (req, res) => {
+    res.send('Utilisateur identifé !');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
